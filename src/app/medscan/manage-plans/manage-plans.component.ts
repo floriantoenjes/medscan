865fa295b2736fcb0c5dbb27fbc30265e0977017
Florian Toenjes <skatepainter@yahoo.de>
2022-03-11 02:45:37 +0100

    Removed unnecessary injection

--- src/app/medscan/manage-plans/manage-plans.component.ts
+++ src/app/medscan/manage-plans/manage-plans.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MedicationPlan} from '../shared/models/medication-plan';
 import {ActivatedRoute, Router} from '@angular/router';
-import {MedicationPlanRepositoryService} from "../shared/repositories/medication-plan-repository.service";
 
 @Component({
   selector: 'app-manage-plans',
@@ -14,7 +13,6 @@ export class ManagePlansComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private planRepository: MedicationPlanRepositoryService,
     private router: Router
   ) {
     this.medicationPlan = this.activatedRoute.snapshot.data['plan'];
